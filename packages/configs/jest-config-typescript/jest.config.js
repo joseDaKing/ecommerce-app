2929be05c8cee388ed58d4b51a402b40bee85c57
joseDaKing <yousefkhalil125@gmail.com>
2022-09-13 13:52:09 +0200

    Added a new jest config for typescript files

---
+++ packages/configs/jest-config-typescript/jest.config.js
@@ -0,0 +1,8 @@
+const jestConfigBase = require("@org/jest-config-base");
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    ...jestConfigBase,
+    preset: "ts-jest",
+    testEnvironment: "node",
+};

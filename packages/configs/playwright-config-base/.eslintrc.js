33bc79de3f7ed6b730cdc95b95ee7382fb8b68ca
joseDaKing <yousefkhalil125@gmail.com>
2022-09-13 08:26:18 +0200

    Updated the @org/playwright-config-base

--- packages/configs/playwright-config-base/.eslintrc.js
+++ packages/configs/playwright-config-base/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    extends: "@org/eslint-config-next",
+    extends: "@org/eslint-config-base",
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: "./tsconfig.json",
     },
     overrides: [
         {
-            files: [".eslintrc.js", "next.config.js"],
+            files: [".eslintrc.js"],
             rules: {
                 "unicorn/prefer-module": "off",
             },

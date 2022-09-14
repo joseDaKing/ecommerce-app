4adff6a701daa2c5df8e06423db1e896c02f3aa5
joseDaKing <yousefkhalil125@gmail.com>
2022-09-14 10:58:06 +0200

    fix(script): updated the scripts

--- packages/configs/lint-staged-config-base/.lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "*.json": "npx prettier",
-    "*.{js,jsx}": "npx eslint",
-    "*.{ts,tsx}": "tsc-files --no-emit",
-    "*.test.ts": ["npx jest", "npx playwright test"],
-};
